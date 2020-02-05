ecdf720bd6bcf09b9bed5e3ee4a5298905552404
Piotr Stateczyński <piotr.stateczynski@inspeerity.com>
2020-02-05 11:41:31 +0100

    Remove @EqualsAndHashCode.Exclude annotations for general consistency.

--- users/src/main/java/com/staxter/userrepository/User.java
+++ users/src/main/java/com/staxter/userrepository/User.java
@@ -3,7 +3,6 @@ package com.staxter.userrepository;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NonNull;
 import lombok.ToString;
 
@@ -13,27 +12,22 @@ import lombok.ToString;
 public class User {
 
     @NonNull
-    @EqualsAndHashCode.Exclude
     private String id;
 
     @NonNull
-    @EqualsAndHashCode.Exclude
     private String firstName;
 
     @NonNull
-    @EqualsAndHashCode.Exclude
     private String lastName;
 
     @NonNull
     private String userName;
 
     // Exclude password wherever possible (shouldn't be exposed!)
-    @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private String plainTextPassword;
 
     @NonNull
-    @EqualsAndHashCode.Exclude
     @ToString.Exclude
     private String hashedPassword;
 }
